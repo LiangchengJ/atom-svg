aa5447aab4026668a2793336000fcb53f052aa65
LiangchengJ <liangchengj@outlook.com>
2021-03-17 22:49:59 +0800

    First commit

---
+++ spec/atom-svg-view-spec.js
@@ -0,0 +1,9 @@
+"use babel";
+
+/* import AtomSvgView from "../lib/atom-svg-view";
+
+describe("AtomSvgView", () => {
+  it("has one valid test", () => {
+    expect("life").toBe("easy");
+  });
+}); */
